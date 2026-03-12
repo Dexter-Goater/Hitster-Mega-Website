a60677f4dbdea2196d06c2d6d2ce693cc990ebdf
Dexter-Goater <23612@burnside.school.nz>
2026-03-13 12:47:34 +1300

    13/03

--- static/javascript/drag.js
+++ static/javascript/drag.js
@@ -1,14 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
 const boxText = document.getElementById("text1")
 const boxes = document.getElementsByClassName("box")
 boxText.addEventListener('dragstart',dragStart)
 boxText.addEventListener('dragend',dragEnd)
 
+for(let box of boxes)
+{
+    box.addEventListener('dragover',dragOver);
+    box.addEventListener('dragenter',dragEnter);
+    box.addEventListener('dragleave',dragLeave);
+    box.addEventListener('drop',Drop);
+}
 function dragStart()
 {
-   console
+   setTimeout(()=>(this.className= 'invisible'),0);
 }
 
 function dragEnd()
 {
-    console.log("Dragged")
+    this.className = 'boxText';
 }
+
+function dragOver(e)
+{
+    e.preventDefault();
+}
+
+function dragEnter(e)
+{
+    e.preventDefault();
+}
+
+function dragLeave(e)
+{
+    e.preventDefault();
+}
+
+function Drop()
+{
+    this.className = 'box';
+    this.append(boxText);
+}
+});
