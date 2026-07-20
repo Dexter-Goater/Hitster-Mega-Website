2fdfd61365c0b1b5b54ddba446761f8eb91f8dd9
Dexter-Goater <23612@burnside.school.nz>
2026-07-20 14:31:07 +1200

    20/07

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort,session,jsonify,request,redirect,url_for
+from flask import Flask, render_template, abort, session, jsonify, request,redirect,url_for
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import sqlite3
